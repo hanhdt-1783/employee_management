0028d2e7545289491be06c0639709df90f1d525d
hanhdt-1783 <doan.thi.hanh@sun-asterisk.com>
2025-10-17 17:53:10 +0700

    Module 1: Getting Started with Spring Boot

--- src/main/java/com/hanhdoan/employee_management/EmployeeManagementApplication.java
+++ src/main/java/com/hanhdoan/employee_management/EmployeeManagementApplication.java
@@ -2,12 +2,19 @@ package com.hanhdoan.employee_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EmployeeManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeeManagementApplication.class, args);
     }
 
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Spring Boot is running!";
+    }
 }
